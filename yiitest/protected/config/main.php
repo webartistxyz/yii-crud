df4502ff976e98dd8a4cc8477aba69778e88f126
webartistxyz <tasnif06@gmail.com>
2022-12-16 20:12:12 +0600

    created yiitest app and uncommented uris

--- yiitest/protected/config/main.php
+++ yiitest/protected/config/main.php
@@ -39,7 +39,6 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -48,7 +47,6 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
